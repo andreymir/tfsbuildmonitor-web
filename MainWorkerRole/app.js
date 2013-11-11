49ba9fc91df7b92c9c716932765419104125ebd2
Andrey Mironov <andrey.mir@gmail.com>
2013-11-12 00:57:06 +0400

    move odata service url into settings

--- MainWorkerRole/app.js
+++ MainWorkerRole/app.js
@@ -3,6 +3,7 @@
  * Module dependencies.
  */
 
+var azure = require('azure');
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
@@ -12,11 +13,19 @@ var app = express();
 var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
-var tfs = require('./tfs').createMonitor(
-  'http://172.16.0.5/odatatfs',
-  'ACE',
-  'ACE-CI',
-  30000);
+var tfs;
+
+azure.RoleEnvironment.getConfigurationSettings(function(error, settings) {
+    if (!error) {
+      console.log('OData TFS: ', settings['odatatfs']);
+
+      tfs = require('./tfs').createMonitor(
+        settings['odatatfs'],
+        'ACE',
+        'ACE-CI',
+        30000);
+    }        
+});
 
 // all environments
 app.set('port', process.env.PORT || 1337);
